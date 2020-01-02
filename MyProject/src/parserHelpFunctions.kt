679462a08d417dd16bd31e26c92dc8ebe0e5cede
daniilxt <daniilxt@github.com>
2020-01-03 00:38:43 +0300

    feat: Created punctuation parser. All parse functions moved to new file.

---
+++ MyProject/src/parserHelpFunctions.kt
@@ -0,0 +1,49 @@
+private val punct_ = arrayListOf<Char>('.', ',', ':', ';', '-')
+fun isPunctuation(ch: Char?): Boolean {
+  return ch in punct_
+}
+
+fun parseWord(str: String?, index: Int): Pair<String, Int> {
+  var line: String = ""
+  var i = index
+
+  while ((str?.get(i)?.isLetter()!!) && (i != str.lastIndex)) {
+    line += str[i]
+    i++
+  }
+  if (i == str.lastIndex && str[i].isLetter()) {
+    line += str.get(i)
+    i++
+  }
+  return Pair(line, i)
+}
+
+fun parseDigit(str: String?, index: Int): Pair<String, Int> {
+  var line: String = ""
+  var i = index
+
+  while ((str?.get(i)?.isDigit()!!) && (i != str.lastIndex)) {
+    line += str[i]
+    i++
+  }
+  if (i == str.lastIndex && str[i].isDigit()) {
+    line += str[i]
+    i++
+  }
+  return Pair(line, i)
+}
+
+fun parsePunct(str: String?, index: Int): Pair<String, Int> {
+  var line: String = ""
+  var i = index
+
+  while ((isPunctuation(str?.get(i))) && (i != str?.lastIndex)) {
+    line += str?.get(i)
+    i++
+  }
+  if (i == str?.lastIndex && isPunctuation(str[i])) {
+    line += str[i]
+    i++
+  }
+  return Pair(line, i)
+}
--- MyProject/src/test1.kt
+++ MyProject/src/test1.kt
@@ -1,13 +1,12 @@
 import java.util.*
+import kotlin.test.todo
 
 class Text() {
   private var text_ = arrayListOf<String>()
-  private val punct_ = arrayListOf<Char>()
-
   fun parseString(str: String?): Unit {
-    if (str != null && str!="") {
+    if (str != null && str != "") {
       var i = 0
-      while (i != str.lastIndex+1) {
+      while (i != str.lastIndex + 1) {
         if (str[i].isLetter()) {
           val tmp = parseWord(str, i)
           text_.add(tmp.first)
@@ -22,8 +21,13 @@ class Text() {
           } else break
         } else if (str[i].isWhitespace()) {
           i++
-        } else if (str[i].isDefined())
-          i++
+        } else if (str[i].isDefined()) {
+          val tmp = parsePunct(str, i)
+          text_.add(tmp.first)
+          if (tmp.second < str.length) {
+            i = tmp.second
+          } else break
+        }
       }
     }
   }
@@ -33,34 +37,4 @@ class Text() {
       println("$i ")
     }
   }
-}
-
-fun parseWord(str: String?, index: Int): Pair<String, Int> {
-  var line: String = ""
-  var i = index
-
-  while ((str?.get(i)?.isLetter()!!) && (i != str.lastIndex)) {
-    line += str[i]
-    i++
-  }
-  if (i == str.lastIndex && str[i].isLetter()) {
-    line += str.get(i)
-    i++
-  }
-  return Pair(line, i)
-}
-
-fun parseDigit(str: String?, index: Int): Pair<String, Int> {
-  var line: String = ""
-  var i = index
-
-  while ((str?.get(i)?.isDigit()!!) && (i != str.lastIndex)) {
-    line += str[i]
-    i++
-  }
-  if (i == str.lastIndex && str[i].isDigit()) {
-    line += str[i]
-    i++
-  }
-  return Pair(line, i)
 }
